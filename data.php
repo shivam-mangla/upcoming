532862a4ffe5440313220a24832b9a6554f27001
Shivam <shivam.mangla.09@gmail.com>
2014-01-29 17:00:43 +0530

    Add comments to data.php and remove useless lines

--- data.php
+++ data.php
@@ -1,23 +1,22 @@
 <?php
-//will check whether the user is logged in or not
-
 include 'core/init.php';
-echo "init";
 
+// to check if any data has been received on this page
 if (empty($_POST) === false){
-	//=== checks for type as well 
-	echo "coming";
-	$name = $_POST['query'];
-	echo 'https://graph.facebook.com/search?q='.$name.'&type=event&center=29.8749,77.8899&distance=100&access_token=CAACEdEose0cBANObVZC5GZBigSgsUv2lPBx7HIUp65OmZBA6znd4sI0oYbf5N93VNkSBZCJ08SnnWmgliUi8gRXFAqqfZARAW3pvbWTNKfitZAWUbndSSxKZAFTtKSZAMxBeqOd25DlNDFKbnGukoTlr5xPw7c2M2F9SZCjC30HaYCZBypdrw28LASa5RHEFiv20MZD';
-	$homepage = file_get_contents('https://graph.facebook.com/search?q='.$name.'&type=event&center=29.8749,77.8899&distance=100&access_token=CAACEdEose0cBANObVZC5GZBigSgsUv2lPBx7HIUp65OmZBA6znd4sI0oYbf5N93VNkSBZCJ08SnnWmgliUi8gRXFAqqfZARAW3pvbWTNKfitZAWUbndSSxKZAFTtKSZAMxBeqOd25DlNDFKbnGukoTlr5xPw7c2M2F9SZCjC30HaYCZBypdrw28LASa5RHEFiv20MZD');
-	$ll = json_decode($homepage);
-print_r($ll);
-	// echo $homepage;
+	//=== checks for type as well
 
-// }else{
-// 	echo 'landed';
-// 	header('Location:index.php');
-// 	exit();
+	// receive the text from query field
+	$name = $_POST['query'];
+
+	echo 'https://graph.facebook.com/search?q='.$name.'&type=event&center=29.8749,77.8899&distance=100&access_token=CAACEdEose0cBANObVZC5GZBigSgsUv2lPBx7HIUp65OmZBA6znd4sI0oYbf5N93VNkSBZCJ08SnnWmgliUi8gRXFAqqfZARAW3pvbWTNKfitZAWUbndSSxKZAFTtKSZAMxBeqOd25DlNDFKbnGukoTlr5xPw7c2M2F9SZCjC30HaYCZBypdrw28LASa5RHEFiv20MZD';
+	// fetch content from the entered url
+	$homepage = file_get_contents('https://graph.facebook.com/search?q='.$name.'&type=event&center=29.8749,77.8899&distance=100&access_token=CAAF5jYUwp3UBAKklyxcLG9JpUlLCe7j8w7W5VywqGYSBk7IAJpswqPWFsMGDFh1VwRqUIyY0ZA7PZBZCgJsZCN2269ZBvn6UQznFLcUtfwSBV0pbiiGQRlc87RDSEpeaEMxsWnClWjkXHfklK3Y5uMhsXtjTP39Re3kCIRbZBzZBMxiQjQuz7sfEy2zKIeAoQoZD');
+	$ll = json_decode($homepage);
+	print_r($ll);
+
+}else{
+	header('Location:index.php');
+	exit();
 }
 
 // if(empty($errors) === false){
