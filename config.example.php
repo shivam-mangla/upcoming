e1326e704cb7ed17f022a66971ba905c9b9eca7d
ankita <ankishukla07@gmail.com>
2014-02-04 00:12:02 +0530

    Added an example configuration file containg the sample parameters

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+  $config = array(
+    'appId' => 'Your_App_Id',
+    'secret' => 'App_Secret',
+    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
+  );
+  $facebook = new Facebook($config);
+  $access_token = $facebook->getAccessToken();
+  ?>
