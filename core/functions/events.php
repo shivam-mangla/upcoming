7e3b0ce3f21b57f74c1b8f3b8d811989c71bde62
Shivam <shivam.mangla.09@gmail.com>
2014-02-13 00:32:43 +0530

    Add code after comments in suggest_time() to get events for the given location and parsed the json using php to convert into array

--- core/functions/events.php
+++ core/functions/events.php
@@ -1,11 +1,32 @@
 <?php
 
 function suggest_time($preferences){
-	var_dump($preferences);
 
 	// get events list using graph api query
+	var_dump($preferences);
+	echo "</br></br></br>";
+	$events_list = get_event_list($preferences["location"]);
+
+	// divide the total time in which event is allowed in intervals of 15 minutes
+	$diff = get_time_difference($preferences["sdate"].$preferences["stime"], 
+												$preferences["edate"].$preferences["etime"]);
+	echo $diff; // 100
+	echo "</br></br></br>";
+
 	// parse the json array
+	$json = json_decode($events_list, true );
+	echo "\n\n\n\nDecoded JSON";
+	var_dump($json);
+	echo "\n\n\n\n";
+
 	// extract time
+	if (isset($json['data']) && is_array($json['data']) ) {
+	        foreach($json['data'] as $result) {
+	              echo 'Title: ' . $result['name'] . '<br />';
+	        }
+	}
+	else echo "\$json['results'] is not an array";
+
 	// ignore events which are not according to preference
 	// find events which satisfy host's datetime preference
 	// define an array for start time to end time in which 15 minutes will define 1 element
@@ -29,7 +50,6 @@ function get_event_list($name){
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	$result = curl_exec($ch);
-	echo "<br><br><br>Result is<br>".$result;
 	curl_close($ch);
 	return $result;
 }
