50e9ec59e439c16420e1cc137ccd06c1bb313601
ankita <ankishukla07@gmail.com>
2014-04-01 18:54:59 +0530

    Sorted the data(events) in (ascending)chronological order

--- includes/widgets/loggedin.php
+++ includes/widgets/loggedin.php
@@ -11,11 +11,14 @@ if (isset($_POST['Search']))
 	$name = $_POST['query'];
 	?>
 	<div id="results">
+	<button type="submit" name="sortby-date" id="sortby-date" >Sort by date</button>
 		<?
 			$result = get_event_list($name);
 			// echo $result;
 			$obj = json_decode($result, true); //parsing json in php
-			function cmp($a, $b)
+			// print_r($obj);
+			//user defined function for sorting data as per the dates
+			function compare($a, $b)
 			{
 				if ($a['start_time'] == $b['start_time'])
 				{
@@ -23,26 +26,71 @@ if (isset($_POST['Search']))
 				}
 				return ($a['start_time'] < $b['start_time']) ? -1 : 1;
 			}
-			uasort($obj['data'], 'cmp');
-			print_r($obj);
+
+			//Sort the events date-wise
+			uasort($obj['data'], 'compare');
+			// print_r($obj);
+			$temp = array();
+			foreach($obj['data'] as $item)
+			{
+				print_r($item);
+				echo "<br>";
+			}
+
+
+
+
+			$tempArray = array_merge($obj);	//Changing the indices of the array back to 0,1,2...
+			// print_r($tempArray);
+			for ($i=0; $i < count($obj['data']) ; $i++) 
+			{ 
+				// echo "<div class='event-details col span_1_of_3'>Name:".$tempArray['data'][$i]['name']."<br>Location:".$tempArray['data'][$i]['location']."<br>Start time:".$tempArray['data'][$i]['start_time']."<br><br></div>";
+			}
+
+
+			
+
+
+
+
 		?>
 	</div>
-	<button onclick="custom_sort()">Sort by date</button>
+	
 	<div class="section group">
 		<div id = "display-output"></div>
 	</div>
 	
 	<script type="text/javascript">
 		var object = (<?=$result;?>);
-    	var frame = document.getElementById('display-output');
-    	for (var i = 0; i < object.data.length; i++)
-    	{
-    		var counter = object.data[i];
-    		var div = document.createElement("div");
-    		div.className = "event-details col span_1_of_3 "
-    		div.innerHTML = "Name:" +(counter.name) +"<br><br>Location:" + (counter.location) + "<br><br>Start time:" + (counter.start_time);
-    		frame.appendChild(div);
-    	}
+		var frame = document.getElementById('display-output');
+		var objectSort = document.getElementById("sortby-date");
+		var simpleSearch = document.getElementById("search");
+		objectSort.addEventListener("click", 
+			var object = (<?=$obj;?>);
+			// show_events();
+			for (var i = 0; i < object.data.length; i++)
+			{
+				// var counter = object.data[i];
+				// var div = document.createElement("div");
+				// div.className = "event-details col span_1_of_3 "
+				// div.innerHTML = "Name:" +(counter.name) +"<br><br>Location:" + (counter.location) + "<br><br>Start time:" + (counter.start_time);
+				// frame.appendChild(div);
+			}
+		);
+		// simpleSearch.addEventListener("click",show_events());
+		// function show_events()
+		// {
+			for (var i = 0; i < object.data.length; i++)
+			{
+				// var counter = object.data[i];
+				// var div = document.createElement("div");
+				// div.className = "event-details col span_1_of_3 "
+				// div.innerHTML = "Name:" +(counter.name) +"<br><br>Location:" + (counter.location) + "<br><br>Start time:" + (counter.start_time);
+				// frame.appendChild(div);
+			}
+		// }
+//     });
+// )};
 	</script>
 
 	<?
@@ -61,7 +109,7 @@ else{
 			<div id="locationField">
 				<input id="autocomplete" type="text" class="input-block-level" name="query" placeholder="Place" onFocus="geolocate()" >
 			</div>
-			<button class="btn btn-large btn-primary" type="submit" name="Search">Search</button>
+			<button class="btn btn-large btn-primary" type="submit" name="Search" id="search">Search</button>
 		</form>
 	</div>
 </div>
@@ -69,12 +117,3 @@ else{
 <?php
 }
 ?>
-<script src="js/masonry.pkgd.min.js"></script>
-// <script>
-// var container = document.querySelector('#container1');
-// var msnry = new Masonry( container, {
-//   // options...
-//   itemSelector: '.item',
-//   columnWidth: 200
-// });
-// </script>
